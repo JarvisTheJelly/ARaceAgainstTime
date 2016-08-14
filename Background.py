b60ceeade1f9fd5ea14b80afab1636e22ba97995
Nick Jarvis <najarvis2016@gmail.com>
2016-08-13 20:38:01 -0400

    Updated background. Dust + Planets + Stars
    Looks decent. Stop chiseling marble.

--- Background.py
+++ Background.py
@@ -25,12 +25,16 @@ class Background(object):
         self.planets = [pygame.image.load('res/Planets/Planet1.png').convert(),
                         pygame.image.load('res/Planets/Planet2.png').convert(),
                         pygame.image.load('res/Planets/Planet3.png').convert()]
+
         for planet in self.planets:
             planet.set_colorkey((255, 0, 255))
 
         self.populate(self.screen_size, True)
 
     def populate(self, screen_size, first=False):
+        """Add all the entities to the background. Entities spawn from
+        the right side of the screen."""
+
         num_entities = self.entity_goal - len(self.entity_list)
         for i in xrange(num_entities):
             if first:
@@ -40,19 +44,29 @@ class Background(object):
             else:
                 random_pos = gametools.vector2.Vector2(screen_size[0], random.randint(0, screen_size[1]))
 
-            is_planet = random.randint(0, 199) == 0
-
-            width = random.uniform(2, 7)
-            distance = (1.0 / (2 ** width)) * (2 ** 7)
-            if not is_planet:
+            if random.randint(0, 299) == 0 or (first and random.randint(0, 9) == 0):
+                # Add a star
+                width = random.uniform(3, 10)
                 random_img = pygame.Surface((int(width), int(width)))
                 random_img.fill((255, 255, 255))
-            else:
+                distance = 75
+
+            elif random.randint(0, 199) == 0:
+                # Add a planet
+                width = random.uniform(2, 7)
                 random_img = random.choice(self.planets)
                 scale = (width-2) / 5
                 random_img = pygame.transform.scale(random_img, (int(random_img.get_width() * scale),
                                                                  int(random_img.get_height() * scale)))
                 random_pos.x += random_img.get_width()
+                distance = (1.0 / (2 ** width)) * (2 ** 7)
+
+            else:
+                # Add a dust particle
+                width = random.uniform(2, 7)
+                random_img = pygame.Surface((int(width), int(width)))
+                random_img.fill((139, 121, 94))
+                distance = (1.0 / (2 ** width)) * (2 ** 3)
 
             entity_to_add = Entity.Entity(random_pos, random_img, distance)
 
@@ -65,7 +79,7 @@ class Background(object):
         speed is how fast the screen is scrolling.
         """
 
-        self.entity_list = sorted(self.entity_list, key=lambda Entity: Entity.img.get_width())
+        self.entity_list = sorted(self.entity_list, key=lambda Entity: -Entity.distance)
 
         garbage_collection = []
         for entity in self.entity_list:
@@ -75,6 +89,9 @@ class Background(object):
 
         for entity in garbage_collection:
             self.entity_list.remove(entity)
+            del entity
+
+        print len(self.entity_list)
 
         self.populate(self.screen_size)
 
